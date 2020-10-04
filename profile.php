cd25f4adea0dccad18d99d45ae673a5241c01d37
Shubh Patel <shubhp18@gmail.com>
2020-10-04 19:53:48 -0400

    Lab 5

--- profile.php
+++ profile.php
@@ -14,6 +14,14 @@ if (!isset($_SESSION['uname'])) {
 require 'includes/header.php';
 require 'includes/footer.php';
 require 'includes/dbhandler.php';
+
+$prof_user = $_SESSION['uname'];
+$sqlpro = "SELECT * FROM profile WHERE uname='$prof_user';";
+
+$res = mysqli_query($conn, $sqlpro);
+$row = mysqli_fetch_array($res);
+$photo = $row['picpath'];
+$bio = $row['bio'];
 ?>
 
 <main>
@@ -37,26 +45,34 @@ require 'includes/dbhandler.php';
         }
     </script>
 
-    <div class="h-100 center-me text-center">
-        <div class="my-auto">
-            <form class="rounded" style="background: var(--brown);"
-                  action="includes/upload-helper.php" method="POST" enctype="multipart/form-data">
-                <div class="form-group">
-                    <img src="" onclick="triggered()" id="prof-display" alt="">
-                    <label for="prof-image" id="uname-style">Dummy Text</label>
-                    <input type="file" name="prof-image" id="prof-image"
-                           onchange="preview(this)" class="form-control" style="display: none">
-                </div>
-                <div class="form-group">
-                <textarea name="bio" id="bio" cols="30" rows="10" placeholder="Bio..."
-                          style="text-align: center"></textarea>
-                </div>
-                <div class="form-group">
-                    <button type="submit" name="prof-submit" class="btn btn-outline-success btn-lg btn-block">
-                        Upload
-                    </button>
-                </div>
-            </form>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 offset-sm-4 my-auto" style="padding-left: 2rem; padding-right: 2rem">
+                <form class="rounded" style="background: var(--brown); height: 25rem;"
+                      action="includes/upload-helper.php" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <img class="mb-3 rounded-circle" src="<?php echo $photo ?>"
+                             onclick="triggered()" alt="uploads/generic.jpg" id="prof-display">
+                        <h4><?php echo $prof_user; ?></h4>
+                        <input type="file" name="prof-image" id="prof-image"
+                               onchange="preview(this)" class="form-control" style="display: none">
+                    </div>
+                    <div class="form-group">
+                    <textarea class="rounded" name="bio" id="bio" rows="4"
+                              placeholder="Bio..." style="text-align: center; width:100%"><?php
+                        if ($bio !== null) {
+                            echo $bio;
+                        }
+                        ?></textarea>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" name="prof-submit" class="btn btn-lg btn-primary btn-block">
+                            Upload
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
+    </div>
 
 </main>
--- signup.php
+++ signup.php
@@ -13,60 +13,56 @@ require 'includes/footer.php';
 <main>
     <link rel="stylesheet" href="css/footer-shadow.css">
     <link rel="stylesheet" href="css/signup.css">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 offset-sm-3">
-                    <div class="my-auto">
-                        <form class="rounded" style="background: var(--brown); height: 37rem;"
-                              action="includes/signup-helper.php" method="post">
-                            <img class="mb-3 rounded-circle" src="images/login/gnome.jpg" alt=""
-                                 style="width: 7.5vw; height: 7.5vw"/>
-                            <h4 class="h4 mb-2 font-weight-normal">Register</h4>
-                            <h6 class="h4 mb-2 font-weight-normal" style="font-size: 1rem">
-                                Create your Garden Gurus account.</h6>
-                            <div class="row form-group">
-                                <div class="col-sm">
-                                    <label for="inputFirstName" class="sr-only">First Name</label>
-                                    <input type="text" class="form-control" required
-                                           id="inputFirstName" name="fname" placeholder="First Name">
-                                </div>
-                                <div class="col-sm">
-                                    <label for="inputLastName" class="sr-only">Last Name</label>
-                                    <input type="text" class="form-control" required
-                                           id="inputLastName" name="lname" placeholder="Last Name">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="inputUsername" class="sr-only">Username</label>
-                                <input type="text" class="form-control" required
-                                       id="inputUsername" name="uname" placeholder="Username">
-                            </div>
-                            <div class="form-group">
-                                <label for="inputEmail" class="sr-only">Email</label>
-                                <input type="email" class="form-control" required
-                                       id="inputEmail" name="email" placeholder="Email">
-                            </div>
-                            <div class="form-group">
-                                <label for="inputPassword" class="sr-only">Password</label>
-                                <input type="password" class="form-control" required
-                                       id="inputPassword" name="password" placeholder="Password">
-                            </div>
-                            <div class="form-group">
-                                <label for="inputConfirmPassword" class="sr-only">Confirm Password</label>
-                                <input type="password" class="form-control" required
-                                       id="inputConfirmPassword" name="confirm_password" placeholder="Confirm Password">
-                            </div>
-                            <div class="form-group">
-                                <button class="btn btn-lg btn-primary btn-block" type="submit"
-                                        style="margin-top: 2rem" name="signup-submit">Register
-                                </button>
-                                <div class="text-muted">Already have an account?
-                                    <a href="login.php" id="sign-in-link">Sign in</a></div>
-                            </div>
-                        </form>
+    <div class="container">
+        <div class="col-sm-6 offset-sm-3 my-auto">
+            <form class="rounded" style="background: var(--brown); height: 37rem;"
+                  action="includes/signup-helper.php" method="post">
+                <img class="mb-3 rounded-circle" src="images/login/gnome.jpg" alt=""
+                     style="width: 7.5vw; height: 7.5vw"/>
+                <h4 class="h4 mb-2 font-weight-normal">Register</h4>
+                <h6 class="h4 mb-2 font-weight-normal" style="font-size: 1rem">
+                    Create your Garden Gurus account.</h6>
+                <div class="row form-group">
+                    <div class="col-sm">
+                        <label for="inputFirstName" class="sr-only">First Name</label>
+                        <input type="text" class="form-control" required
+                               id="inputFirstName" name="fname" placeholder="First Name">
+                    </div>
+                    <div class="col-sm">
+                        <label for="inputLastName" class="sr-only">Last Name</label>
+                        <input type="text" class="form-control" required
+                               id="inputLastName" name="lname" placeholder="Last Name">
                     </div>
                 </div>
-            </div>
+                <div class="form-group">
+                    <label for="inputUsername" class="sr-only">Username</label>
+                    <input type="text" class="form-control" required
+                           id="inputUsername" name="uname" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <label for="inputEmail" class="sr-only">Email</label>
+                    <input type="email" class="form-control" required
+                           id="inputEmail" name="email" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword" class="sr-only">Password</label>
+                    <input type="password" class="form-control" required
+                           id="inputPassword" name="password" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <label for="inputConfirmPassword" class="sr-only">Confirm Password</label>
+                    <input type="password" class="form-control" required
+                           id="inputConfirmPassword" name="confirm_password" placeholder="Confirm Password">
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-lg btn-primary btn-block" type="submit"
+                            style="margin-top: 2rem" name="signup-submit">Register
+                    </button>
+                    <div class="text-muted">Already have an account?
+                        <a href="login.php" id="sign-in-link">Sign in</a></div>
+                </div>
+            </form>
         </div>
+    </div>
 
 </main>
